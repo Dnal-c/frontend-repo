a1a405347453ef052b6bc9a5a5527b4749d46652
Alexey-Alexeev <alexeevv-alexey@yandex.ru>
2024-06-12 21:51:28 +0300

    add mobile version for Main and SearchFeed page

---
+++ src/pages/SeacrhFeed/SearchFeed.jsx
@@ -0,0 +1,49 @@
+import React, {Fragment, useEffect, useState} from 'react';
+import {useSearchParams} from 'react-router-dom';
+import {Box} from '@mui/material';
+import VideoItem from '../../components/Videos/VideoItem';
+import {getSearchFeed} from '../../api/searchFeed';
+import {theme} from './../../utils/theme';
+
+const SearchFeed = () => {
+    const [searchParams] = useSearchParams();
+    const query = searchParams.get('q');
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        getSearchFeed(query).then((result) => setData(result));
+    }, [query]);
+
+    return (
+        <Box
+            sx={{
+                margin: '130px 20% 0',
+                overflow: 'auto',
+                [theme.breakpoints.down('md')]: {
+                    margin: '15% 1%',
+                },
+                [theme.breakpoints.down('sm')]: {
+                    margin: '23% 1% 15%',
+                },
+            }}
+        >
+            {data.map((item, idx) => {
+                return (
+                    <Fragment key={idx}>
+                        <Box
+                            sx={{
+                                color: '#fff',
+                                width: '100%',
+                                margin: '0 auto',
+                            }}
+                        >
+                            <VideoItem key={item.link} {...item} />
+                        </Box>
+                    </Fragment>
+                );
+            })}
+        </Box>
+    );
+};
+
+export default SearchFeed;
