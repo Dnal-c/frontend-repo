917428b53734c0a1c4f016ba17f46b27fe5999bb
Alexey-Alexeev <alexeevv-alexey@yandex.ru>
2024-06-16 01:33:14 +0300

    change url

--- src/api/upload.js
+++ src/api/upload.js
@@ -1,11 +1,11 @@
 import { $uploadApi } from '../utils/common';
 
 export async function postPredictByFile(params) {
-    const res = await $uploadApi.post('api/v1/calculate-predict-by-file', params);
+    const res = await $uploadApi.post('predict-by-file', params);
     return res;
 }
 
 export async function postPredictByLink(params) {
-    const res = await $uploadApi.post('/api/v2/calculate-predict-by-link', params);
+    const res = await $uploadApi.post('index', params);
     return res;
 }
